f8abcb5cc7e27610dbd68e65932ea78b8032d2f3
yogesh <etpr.yogesh@gmail.com>
2021-10-08 11:52:56 +0530

    answeres to the basics questions asked

---
+++ basics.js
@@ -0,0 +1,30 @@
+//1- the time complexity of reading an element in an array should be O(n) which should be O(1) in this case as we are reading a single array
+
+//2- it should be o(1) as those are single operations
+
+//3-well the answere can be dependent on the input also but for linked list sorting, merge sort can be a best option with o(n logn) time complexity
+
+//5- hashmap and hash tables are sort or similar in this case and what happen is that the key which is added will be teking the place of the present key, and previous key will no longer will be availabile.
+
+//6-yes we can delete the middle element of an odd length stack,first we need to find its length and then find which is the middle element
+//suppose the stack length is 9 and we can not devide it by 2 so we add 1 and devide the stacl length by 2 and the number we have is the middle
+// of the stack. that is 5th if the stack length is 9
+//11 is the stack length is 21
+//  and use splice methode to remove it.
+
+//7- in javascript we use parseFloat()for a decimal type of value to store in a varible but if we want specificaly only 2
+// decimal after the "." thenwe have to user toFixed() methode in the variable
+var a = parseFloat("5.888888").toFixed(2)
+//will return var a=5.88
+
+
+//8- yes we can create a circular linked list with a doubly linked list or normal linked list,
+//in which we put the address of first node in last node (right side)address
+
+//9-for the working of recursive function to work , we use last in first out stack which is also known as LIFO
+
+//10-overflow in a bucket cause when it overflows,and the the term overflows means when the key and value pair are full
+//and the input is still coming
+
+//11-the nested loops are loops withing loops which are not the best practice but yet we have to use them at various situations,
+//so where a loop time complexity is O(n), the nested loop time complexity is O(n2) which is O n square
